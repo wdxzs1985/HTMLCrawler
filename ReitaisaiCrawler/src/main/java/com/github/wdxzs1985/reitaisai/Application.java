b5bb2bf1e4289c796afae955cfd24b64a2ee2c75
wdxzs1985 <wdxzs1985@gmail.com>
2014-04-22 14:34:30 +0900

    update

--- ReitaisaiCrawler/src/main/java/com/github/wdxzs1985/reitaisai/Application.java
+++ ReitaisaiCrawler/src/main/java/com/github/wdxzs1985/reitaisai/Application.java
@@ -1,196 +1,40 @@
 package com.github.wdxzs1985.reitaisai;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringEscapeUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.github.wdxzs1985.html.Crawler;
-
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application implements CommandLineRunner {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Value("${url:http://reitaisai.com/circlelist/circlename/}")
-    private String url;
+    public static final String[] splitCsv(String line) {
+        List<String> splited = new ArrayList<String>();
+        StringBuilder strB = new StringBuilder();
+        char strChar = 0;
+        boolean isString = false;
 
-    @Value("${htmlfile:circle.html}")
-    private String htmlfile;
-
-    @Value("${csvfile:circle.csv}")
-    private String csvfile;
-
-    @Value("${sortByName:circlename.json}")
-    private String sortByName;
-
-    @Value("${sortByPos:circlepos.json}")
-    private String sortByPos;
-
-    @Value("${crawl:true}")
-    private boolean crawl;
-
-    private final Crawler crawler = new Crawler();
-
-    private final Log log = LogFactory.getLog(this.getClass());
-
-    private final Pattern TABLE_PATTERN = Pattern.compile("<h3 id=\"(.)\">.</h3><table class=\"circlelist\">(.*?)</table>");
-    private final Pattern ITEM_PATTERN = Pattern.compile("<tr class=\".*?\"><td>(.*?)</td><td>(.*?)</td><td>(.)([0-9]{2})([ab])</td></tr>");
-
-    @Override
-    public void run(String... arg0) throws Exception {
-        this.parseHtml();
-        this.parseCsv();
-    }
-
-    private String getHtml() throws Exception {
-        File file = new File(this.htmlfile);
-        String html = null;
-        if (this.crawl) {
-            html = this.crawler.getHtml(this.url);
-            FileUtils.write(file, html);
-        } else {
-            html = FileUtils.readFileToString(file);
-        }
-        return html.replaceAll("\\r\\n[\\t\\s]*|\\r[\\t\\s]*|\\n[\\t\\s]*", "");
-    }
-
-    private void parseHtml() throws Exception {
-        List<String> lines = new ArrayList<String>();
-        String html = this.getHtml();
-        this.parseTable(html, lines);
-        this.outputCSV(lines);
-    }
-
-    private void parseTable(String html, List<String> lines) {
-        Matcher matcher = this.TABLE_PATTERN.matcher(html);
-        while (matcher.find()) {
-            String alpha = matcher.group(1);
-            String table = matcher.group(2);
-            this.parseTableRow(alpha, table, lines);
-        }
-    }
-
-    private void parseTableRow(String alpha, String table, List<String> lines) {
-        Matcher matcher = this.ITEM_PATTERN.matcher(table);
-        while (matcher.find()) {
-            String circleName = matcher.group(1);
-            String penName = matcher.group(2);
-            String position1 = matcher.group(3);
-            String position2 = matcher.group(4);
-            String position3 = matcher.group(5);
-            lines.add(String.format("%s,%s,%s,%s,%s,%s,,",
-                                    alpha,
-                                    StringEscapeUtils.unescapeHtml4(circleName),
-                                    StringEscapeUtils.unescapeHtml4(penName),
-                                    position1,
-                                    position2,
-                                    position3));
-        }
-    }
-
-    private void outputCSV(List<String> lines) throws IOException {
-        FileUtils.writeLines(new File(this.csvfile), lines);
-    }
-
-    private void parseCsv() throws IOException {
-        List<String> lines = FileUtils.readLines(new File(this.csvfile));
-        this.outputSortByName(lines);
-        this.outputSortByPos(lines);
-    }
-
-    private void outputSortByName(List<String> lines) throws IOException {
-        String key = null;
-        JSONObject data = new JSONObject();
-        JSONArray keymap = new JSONArray();
-        for (String circle : lines) {
-            String[] sa = StringUtils.splitPreserveAllTokens(circle, ",");
-            key = sa[0];
-            JSONArray group = data.optJSONArray(key);
-            if (group == null) {
-                group = new JSONArray();
-                keymap.add(key);
+        for (int i = 0; i < line.length(); i++) {
+            strChar = line.charAt(i);
+            if (strChar == "\"".charAt(0)) {
+                isString = isString ? false : true;
+            } else if (!isString && strChar == ",".charAt(0)) {
+                splited.add(strB.toString());
+                strB = new StringBuilder();
+            } else {
+                strB.append(strChar);
             }
-            group.add(circle);
-            data.put(key, group);
         }
-        data.put("keymap", keymap);
-
-        FileUtils.write(new File(this.sortByName),
-                        "var data = " + data.toString());
-    }
-
-    private void outputSortByPos(List<String> lines) throws IOException {
-
-        Collections.sort(lines, new Comparator<String>() {
-
-            @Override
-            public int compare(String o1, String o2) {
-                String[] s1 = StringUtils.splitPreserveAllTokens(o1, ",");
-                String[] s2 = StringUtils.splitPreserveAllTokens(o2, ",");
-
-                String position11 = s1[3];
-                String position21 = s2[3];
-                if (position11.charAt(0) != position21.charAt(0)) {
-                    return position11.charAt(0) - position21.charAt(0);
-                }
-
-                String position12 = s1[4];
-                String position22 = s2[4];
-                if (Integer.parseInt(position12) != Integer.parseInt(position22)) {
-                    return Integer.parseInt(position12) - Integer.parseInt(position22);
-                }
-
-                String position13 = s1[5];
-                String position23 = s2[5];
-                if (position13.charAt(0) != position23.charAt(0)) {
-                    return position13.charAt(0) - position23.charAt(0);
-                }
-
-                return 0;
-            }
-        });
-
-        String key = null;
-        JSONObject data = new JSONObject();
-        JSONArray keymap = new JSONArray();
-        for (String circle : lines) {
-            String[] sa = StringUtils.splitPreserveAllTokens(circle, ",");
-            key = sa[3];
-            JSONArray group = data.optJSONArray(key);
-            if (group == null) {
-                group = new JSONArray();
-                keymap.add(key);
-            }
-            group.add(circle);
-            data.put(key, group);
-        }
-        data.put("keymap", keymap);
-
-        FileUtils.write(new File(this.sortByPos),
-                        "var data = " + data.toString());
+        splited.add(strB.toString());
+        return splited.toArray(new String[] {});
     }
 }
